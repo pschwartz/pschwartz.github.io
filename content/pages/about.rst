1b81a3e2cf7e2888f01ccff31a86e14e684f36ee
Philip Schwartz <philip.schwartz@rackspace.com>
2016-01-03 23:11:49 -0500

    New site
    Signed-off-by: Philip Schwartz <philip.schwartz@rackspace.com>

---
+++ content/pages/about.rst
@@ -0,0 +1,42 @@
+About
+#####
+
+:title: About
+
+.. _IBM: http://www.ibm.com
+.. _OpenStack: http://www.openstack.org
+.. _Ansible: http://www.ansible.com
+__ IBM_
+
+------------
+
+.. image:: {filename}/images/me.jpg
+   :alt: me-picture
+   :align: right
+   :class: .me-picture
+
+My name is Philip Marc Schwartz, I am a Hacker. No, not the media misconception of a cyber criminal breaking into computers to steal valuable information, but a classic hacker. I am an artist of code, I am a programmer.  I am proficent in a multitude of programming languages including C, C++, Java, Perl, Python; along with many others both common and uncommon. I am an expert of systems automation with both Ansible_ and Puppet. I am also an avid photographer and a devoted husband/father.
+
+
+I currently work as a Senior Software Engineer for `IBM Cloud US`__ working on OpenStack_ development.
+
+My focus is primarily on OpenStack Infrastructure development, but I am also involved with Nova and Neutron development. 
+
+I am also an active contributor to the Ansible_ project.
+
+------------
+
+.. csv-table:: Work History
+   :header: "Title", "Company", "Date"
+   :class: .table-hover
+
+   "Senior Software Engineer, OpenStack Development", "IBM Cloud US", "October 2015 - Present"
+   "Software Developer 3, OpenStack Development", "Rackspace", "July 2014 - October 2015"
+   "DevOps Engineer 3, OpenStack Deployments", "Rackspace", "July 2013 - July 2014"
+   "Senior Software Engineer, HPCC Platform", "LexisNexis RS", "September 2008 - July 2013"
+   "Software Engineer, HPCC Platform", "LexisNexis RS", "June 2007 - September 2008"
+   "Systems Developer, Shared Hosting", "HostWay", "March 2005 - May 2007"
+   "Applications Developer, Control Panels", "Affinity Internet", "January 2003 - March 2005"
+   "Systems Administration 3, Shared Hosting", "Affinity Internet", "June 2002 - January 2003"
+   "Systems Administration 2, Dedicated Hosting", "Affinity Internet", "August 2001 - June 2002"
+   "Systems Administration 1, Dedicated Hosting", "Affinity Internet", "March 2001 - August 2001"
---
+++ pelicanconf.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+import os
+import sys
+sys.path.append(os.curdir)
+
+AUTHOR = u'Philip Marc Schwartz'
+AUTHOR_EMAIL = u'philip@progmad.com'
+TWITTER_USERNAME = 'xhypno402'
+SITENAME = u'Stalking the Programing world'
+SITEURL = u'http://localhost:8000'
+
+PATH = 'content'
+
+MD_EXTENSIONS = ['codehilite(css_class=highlight)', 'extra', 'headerid']
+
+PLUGIN_PATHS = ['pelican-plugins']
+PLUGINS = [
+    'assets',
+    'sitemap',
+    'gravatar',
+    'related_posts',
+    'series',
+    'tipue_search',
+    'liquid_tags.img',
+    'liquid_tags.video',
+    'liquid_tags.youtube',
+    'liquid_tags.vimeo',
+    'liquid_tags.include_code',
+    'tag_cloud',
+]
+
+DIRECT_TEMPLATES = [
+    'archives',
+    'authors',
+    'categories',
+    'index',
+    'search',
+    'tags',
+    'tipue_search',
+]
+
+TIPUE_SEARCH_SAVE_AS = 'tipue_search.json'
+
+# Theme related values
+THEME = 'pelican-bootstrap3'
+BOOTSTRAP_THEME = 'flatly'
+PYGMENTS_STYLE = "native"
+# BOOTSTRAP_FLUID = True
+DISPLAY_BREADCRUMBS = True
+# DISPLAY_CATEGORY_IN_BREADCRUMBS = True
+BOOTSTRAP_NAVBAR_INVERSE = True
+RELATED_POSTS_MAX = 10
+DISPLAY_ARTICLE_INFO_ON_INDEX = False
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+DISPLAY_RECENT_POSTS_ON_SIDEBAR = False
+RECENT_POST_COUNT = 5
+DISPLAY_TAGS_INLINE = True
+TAG_CLOUD_MAX_ITEMS = 10
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = True
+DISPLAY_SERIES_ON_SIDEBAR = True
+SHOW_SERIES = True
+DOCUTIL_CSS = True
+#CC_LICENSE = "CC-BY-NC-SA"
+
+SHOW_ARTICLE_AUTHOR = True
+SHOW_DATE_MODIFIED = True
+
+CUSTOM_CSS = 'static/custom.css'
+
+STATIC_PATHS = [
+    'images',
+    'files',
+    'extra/robots.txt',
+    'extra/custom.css',
+    'extra/CNAME',
+]
+
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/custom.css': {'path': 'static/custom.css'},
+    'extra/CNAME': {'path': 'CNAME'}
+}
+
+# Addthis
+ADDTHIS_PROFILE = u'ra-568734d5aaae4d18'
+
+# GITHUB_USER = u'pschwartz'
+# GITHUB_SKIP_FORK = True
+
+TIMEZONE = 'US/Eastern'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+LINKS = None
+
+# Social widget
+SOCIAL = (
+    ('twitter', 'https://twitter.com/xhypno402'),
+    ('github', 'http://github.com/pschwartz'),
+    ('google-plus', 'https://plus.google.com/+PhilipSchwartzRax'),
+    ('linkedin', 'https://www.linkedin.com/in/philip-schwartz-a18126a'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+PAGE_URL = '{slug}.html'
+PAGE_SAVE_AS = '{slug}.html'
+TAG_URL = 'tags/{slug}.html'
+TAG_SAVE_AS = 'tags/{slug}.html'
+TAGS_URL = 'tags.html'
+
+DEFAULT_DATE_FORMAT = ('%d/%b/%Y %a')
+ARTICLE_URL = "{date:%Y}/{date:%m}/{slug}/"
+ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{slug}/index.html"
+
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+# AVATAR = 'https://avatars1.githubusercontent.com/u/468398?v=3&s=460'
